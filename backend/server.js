3f1ca455e72ba92702f81d05bfe94c0be57518b1
Kanishka Dhodduraj <kanishkadhodduraj2007@gmail.com>
2026-09-24 14:25:49 +0530

    feat: connect backend with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Food Ordering Backend Running");
 });
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed");
+         console.log(error.message);
+    });
