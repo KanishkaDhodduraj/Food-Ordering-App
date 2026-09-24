df47a3ea2e7251596eac6323c46250f5463e2412
Kanishka Dhodduraj <kanishkadhodduraj2007@gmail.com>
2026-09-24 14:32:20 +0530

    feat: add user registration API

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors());
+
+const userRoutes = require("./routes/userRoutes");
+
 app.use(express.json());
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
     res.send("Food Ordering Backend Running");
 });
